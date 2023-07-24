dd81c3ed4c5471eb8b81de1fb2ad6f98d598acdd
dhiraj <dhirajkhodade@gmail.com>
2023-07-24 16:33:12 +0900

    Initial commit

---
+++ test/container-lambda-python-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ContainerLambdaPythonApi from '../lib/container-lambda-python-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/container-lambda-python-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ContainerLambdaPythonApi.ContainerLambdaPythonApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
